7607dfa4a8f5c4f8a10f1febb9f9ccfd0a726a52
Bart Prokop <prokop.bart@gmail.com>
2017-10-12 00:37:14 +0100

    Working NIO

---
+++ undertow-offline/EntryPoint.java
@@ -0,0 +1,17 @@
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+public class EntryPoint {
+
+    public static void main(String... args) {
+        Undertow.Builder undertowBuilder = Undertow.builder();
+        undertowBuilder.addHttpListener(8080, "localhost");
+        undertowBuilder.setHandler(e -> {
+            e.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+            e.getResponseSender().send("Hello World");
+        });
+        Undertow server = undertowBuilder.build();
+        server.start();
+    }
+
+}
